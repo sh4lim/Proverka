f33759239b55355f5729fe00d2704ed1c00000b4
rkore <rkore@DESKTOP-TOU0Q2O>
2024-12-18 19:30:25 +0300

    Добавьте файлы проекта.

---
+++ Core/ProverkaType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleType.Core
+{
+    internal class ProverkaType
+    {
+        public int Proverka(int i, string s) 
+        {
+
+            start:
+
+            Console.Write(s);
+            if (int.TryParse(Console.ReadLine(), out i))
+            {
+                return i;
+            }
+            else 
+            {
+                goto start;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleType.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleType
+{
+    internal class Program
+    {
+        static Random rand = new Random();
+
+        static int max;
+        static int min;
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                start:
+                ProverkaType proverkaType = new ProverkaType();
+
+                min = proverkaType.Proverka(min, "Введите мин число =>");
+
+                max = proverkaType.Proverka(max, "Введите макс число =>");
+
+                Console.Write($"\nМин:{min} Макс:{max} \n\n");
+
+                if( min > max ) { Console.WriteLine("Минимальное число не может быть больше максимального!\n"); goto start; }
+
+                Console.Write($"\n{rand.Next(min, max)}\n\n");
+            }
+
+        }
+    }
+}
